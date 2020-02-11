473cfbb712f7308792bb6415d0722d19005c13b0
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2020-02-11 13:36:57 +0100

    mix.exs: Add configuration to allow build with Elixir apps

---
+++ mix.exs
@@ -0,0 +1,13 @@
+defmodule HostTriple.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :host_triple,
+      description: "A library that produces uniform host triples",
+      version: "0.1.0",
+      language: :erlang,
+      deps: []
+    ]
+  end
+end
